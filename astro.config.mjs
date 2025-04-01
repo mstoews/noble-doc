69d1e33d07cb183f047411220ac9495d16b4489e
mstoews <mstoews@hotmail.com>
2025-04-01 10:53:15 -0400

    update images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,65 +1,66 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import mdx from '@astrojs/mdx';
-import icon from 'astro-icon';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-import starlightBlog from 'starlight-blog'
+import starlightBlog from "starlight-blog";
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        // @ts-ignore
-        plugins: [tailwindcss()],
-    },
-    integrations: [sitemap(), icon({
-        include: {
-          tabler: ['*'],
-          'flat-color-icons': [
-            'template',
-            'gallery',
-            'approval',
-            'document',
-            'advertising',
-            'currency-exchange',
-            'voice-presentation',
-            'business-contact',
-            'database',
-          ],
-        },
-        }), starlight({
-        plugins: [starlightBlog()],
-        title: 'NBL Documentation',                
-        logo: {
-            src: './src/assets/chess.jpg',
-            alt: 'Noble Ledger Logo',
-        },
-        sidebar: 
-        [	
-            {
-                label: 'Start Here',
-                autogenerate: { directory: 'getting_started' },
-            },			
-            {
-                label: 'Noble Ledger Docs',
-                autogenerate: { directory: 'noble_ledger' },
-            },				
-            {
-                label: 'Accounting',
-                autogenerate: { directory: 'accounting' },					
-            },
-            {
-                label: 'Guides',
-                autogenerate: { directory: 'guides' },					
-            },
-            {
-                label: 'Company Policies',
-                autogenerate: { directory: 'policy' },					
-            },				
+  vite: {
+    // @ts-ignore
+    plugins: [tailwindcss()],
+  },
+  integrations: [
+    sitemap(),
+    icon({
+      include: {
+        tabler: ["*"],
+        "flat-color-icons": [
+          "template",
+          "gallery",
+          "approval",
+          "document",
+          "advertising",
+          "currency-exchange",
+          "voice-presentation",
+          "business-contact",
+          "database",
         ],
-        })
-        , mdx(), 
-        ],
-    
+      },
+    }),
+    starlight({
+      plugins: [starlightBlog()],
+      title: "NBL Documentation",
+      logo: {
+        src: "./src/assets/astronaut.png",
+        alt: "Noble Ledger Logo",
+      },
+      sidebar: [
+        {
+          label: "Start Here",
+          autogenerate: { directory: "getting_started" },
+        },
+        {
+          label: "Noble Ledger Docs",
+          autogenerate: { directory: "noble_ledger" },
+        },
+        {
+          label: "Accounting",
+          autogenerate: { directory: "accounting" },
+        },
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+        },
+        {
+          label: "Company Policies",
+          autogenerate: { directory: "policy" },
+        },
+      ],
+    }),
+    mdx(),
+  ],
 });
